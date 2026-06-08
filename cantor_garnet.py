caebc6bef14b2718c1272e40c60eb3b9fb0317e2
knierzy <42247124+knierzy@users.noreply.github.com>
2026-06-08 13:24:07 +0200

    Update cantor_garnet.py

--- cantor_garnet.py
+++ cantor_garnet.py
@@ -152,15 +152,15 @@ def add_rechtecke_mit_farbverlauf(rechtecke, x_offset, spiegeln=False):
                 showlegend=False
 ))
 
-       # for x_pos in range(1, breite):
-       #     x_val = x_offset - x_pos if spiegeln else x_offset + x_pos
-       #     fig.add_trace(go.Scatter(
-       #         x=[x_val, x_val],
-     #           y=[y_position, y_position + hoehe],
-    #            mode="lines",
-     #           line=dict(color="gray", width=2),
-        #         showlegend=False
-      #      ))
+        for x_pos in range(1, breite):
+            x_val = x_offset - x_pos if spiegeln else x_offset + x_pos
+            fig.add_trace(go.Scatter(
+                x=[x_val, x_val],
+               y=[y_position, y_position + hoehe],
+               mode="lines",
+                line=dict(color="gray", width=2),
+                 showlegend=False
+           ))
 
 # === Create axis labels (GLOBAL, NOT inside function!) ===
 x_labels = {
